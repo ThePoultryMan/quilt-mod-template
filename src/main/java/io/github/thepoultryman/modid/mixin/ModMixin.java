25819613c05092092e0db0f95330042415ac6ff5
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-04-22 22:04:21 -0400

    yeah

--- src/main/java/io/github/thepoultryman/modid/mixin/ModMixin.java
+++ src/main/java/io/github/thepoultryman/modid/mixin/ModMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package io.github.thepoultryman.modid.mixin;
 
-import com.example.example_mod.ExampleMod;
+import io.github.thepoultryman.modid.ModClass;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
+public class ModMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ModClass.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
